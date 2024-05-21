d48f11b9e72ac7b80cff606be41b8d170c0f0442
rajkumarinfomagnus <rajkumar.bommaraju@infomagnus.com>
2024-05-21 17:33:14 +0530

    added app.js, welcome.js and server.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Welcome from './welcome';
+
+function App() {
+    return (
+        <div className="App">
+            <Welcome name="GitHub Copilot" />
+        </div>
+    );
+}
+
+export default App;
